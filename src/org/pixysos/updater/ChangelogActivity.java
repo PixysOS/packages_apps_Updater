83e69fffd22e64d54717ff9d9f60e0a300664dfe
AxelBlaz3 <karthikgaddam4@gmail.com>
2019-12-17 20:51:02 +0530

    Migrate to AndroidX
    Signed-off-by: AxelBlaz3 <karthikgaddam4@gmail.com>
    Change-Id: Ie5934baae4ee3e6446f650e5cf0e58facd1d4760

--- src/org/pixysos/updater/ChangelogActivity.java
+++ src/org/pixysos/updater/ChangelogActivity.java
@@ -33,8 +33,8 @@ package org.pixysos.updater;
 
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
