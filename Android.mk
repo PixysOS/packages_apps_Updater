654878f860a11c05c6367a2718656c8903831efa
Psycho Mods <nishsin26@gmail.com>
2019-12-19 01:12:41 +0530

    AndroidX compatible Makefile
    Change-Id: I9acb11320cd8189af682312626fceffdec398357

--- Android.mk
+++ Android.mk
@@ -9,13 +9,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-design \
-    android-support-transition \
-    android-support-v4 \
-    android-support-v7-appcompat \
-    android-support-v7-cardview \
-    android-support-v7-preference \
-    android-support-v7-recyclerview
+    androidx.appcompat_appcompat \
+    androidx.cardview_cardview \
+    com.google.android.material_material \
+    androidx.preference_preference \
+    androidx.recyclerview_recyclerview
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res
